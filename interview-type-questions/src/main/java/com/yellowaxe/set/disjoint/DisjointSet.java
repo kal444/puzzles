a8b86bdab77078d90de473ba050db1cb02e04f7a
Kyle Huang <kyle@yellowaxe.com>
2015-10-30 18:46:21 -0500

    moved interview type questions here

---
+++ interview-type-questions/src/main/java/com/yellowaxe/set/disjoint/DisjointSet.java
@@ -0,0 +1,21 @@
+package com.yellowaxe.set.disjoint;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by kal on 12/23/13.
+ *
+ * calculate the "disjoint sets" of a collection of sets; in other words,
+ * given sets of { 1, 2, 3 }, { 1, 2, 4 } and { 1, 2, 5 }, the result should
+ * be sets of {1,2},{3},{4}, and {5}. Do this and calculate the big-O
+ * notation for your solution in terms of time and of space/memory.
+ */
+public class DisjointSet {
+
+}
+
